aa47318073b6518743ab653f9d012ea984073b59
JKulczynski <j.kulczynski@adform.com>
2022-05-10 14:58:56 +0200

    Second Commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,19 @@
 import java.io.File
 
 object Main extends App {
-def getListOfFiles(dir:String) = {
-  val file = new File(dir)
+  def getListOfFiles(dir: String) = {
+    val file = new File(dir)
 
-  if (file.exists && file.isDirectory){
-  file
-    .listFiles
-    .filter(_.isFile)
-    .map(_.getName)
-    .toList
-    .foreach(println)}
-  else List[File]
-}
+    if (file.exists && file.isDirectory) {
+      file
+        .listFiles
+        .filter(_.isFile)
+        .map(_.getName)
+        .toList
+        .foreach(println)
+    }
+    else List[File]
+  }
+
   println(getListOfFiles(args(0)))
 }
